c1f561dd2effa78f59de102be8681b2c55df38ef
SilviaBec <silvibecerra681@gmail.com>
2023-03-14 18:54:03 -0500

    Avance corrección lógica

--- PS2.js
+++ PS2.js
@@ -1,20 +1,20 @@
 function breakingRecords(scores) {
     // Write your code here
-    contadorMin=0
-    contadorMax=0
-
+    var acumMax=0
+    var acumMin=0
+    
     for(const x of scores){
-        if(x==scores.min || x<scores.min){
-            contadorMin=contadorMin+1
+        if(x<scores[scores.indexOf(x)+1]){
+            acumMin=acumMin+1
         }
-        if(x==scores.max || x<scores.max){
-            contadorMax=contadorMax+1
-// x<scores[scores.indexOf(x)+1]
+        if(x>scores[scores.indexOf(x)+1]){
+            acumMax=acumMax+1
         }
 
     }
-
-    console.log(contadorMax,contadorMin)
+    rta.push(acumMax,acumMin)
+    return rta
+    
 
 }
 breakingRecords([10,5,20,20,4,5,2,25,1])
