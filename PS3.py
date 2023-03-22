ddfa53715317f830230f797e071b81ad83f4a8c1
SilviaBec <silvibecerra681@gmail.com>
2023-03-22 09:27:46 -0500

    Intento arreglar rango

--- PS3.py
+++ PS3.py
@@ -1,12 +1,37 @@
 def birthday(s, d, m):
     resultado2=[]
     for x in s:
-        if x+s[s.index(x)+1] ==d:
-            resultado =x,s[s.index(x)+1]
-            if resultado not in resultado2:
-                resultado2.append(resultado)
-    if len(resultado2)==m:
-        print(len(resultado2))
+        for i in range(1,len(s)+1):
+            if x+s[s.index(x)+i] ==d:
+                resultado =x,s[s.index(x)+i]
+                if resultado not in resultado2:
+                    resultado2.append(resultado)
+                if len(resultado2)==m:
+                    print(len(resultado2))
+
+# def birthday(s, d, m):
+#     resultado2=[]
+#     for x in s:
+#         if x==d and len(resultado2)==m:
+#                 print(m)
+#         else: 
+#             while x<d:
+#                 for i in range(1,len(s)+1):
+#                     if x+s[s.index(x)+i] ==d:
+#                         pass
+
+                        
+#             resultado =x,s[s.index(x)+1]
+#             if resultado not in resultado2:
+#                 resultado2.append(resultado)
+#     if len(resultado2)==m:
+#         print(len(resultado2))
+
+            
+
         
         
-birthday([1,2,1,3,2],3,2)
+birthday([1,1,1,1,1,1],3,2)
+#[1,2,1,3,2],3,2
+# ([4],4,1)
+#([1,1,1,1,1,1],3,2)
