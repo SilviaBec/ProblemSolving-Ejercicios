4c1d908fd80c69ebf6ac0673fd53b97dbb72ddf7
SilviaBec <silvibecerra681@gmail.com>
2023-03-24 19:07:18 -0500

    Tercer intento

--- PS3.py
+++ PS3.py
@@ -1,37 +1,30 @@
+# a=[1,1,1,1,1,1]
+# dia=3
+# acumulado=0
+
+# for x in a:
+#         while acumulado<dia:
+#             acumulado=acumulado+x
+#             print(acumulado)
+
+
+
 def birthday(s, d, m):
     resultado2=[]
-    for x in s:
-        for i in range(1,len(s)+1):
-            if x+s[s.index(x)+i] ==d:
-                resultado =x,s[s.index(x)+i]
-                if resultado not in resultado2:
-                    resultado2.append(resultado)
-                if len(resultado2)==m:
-                    print(len(resultado2))
+    acumulado=0
+    if s[0]==d:
+        print(m)
+    else:
+        while acumulado<d:
+            for x in s:
+                for i in range(len(s)):
+                    # acumulado = 
+                    acumulado = acumulado +(x+s[i])
+    print(acumulado)
 
-# def birthday(s, d, m):
-#     resultado2=[]
-#     for x in s:
-#         if x==d and len(resultado2)==m:
-#                 print(m)
-#         else: 
-#             while x<d:
-#                 for i in range(1,len(s)+1):
-#                     if x+s[s.index(x)+i] ==d:
-#                         pass
-
-                        
-#             resultado =x,s[s.index(x)+1]
-#             if resultado not in resultado2:
-#                 resultado2.append(resultado)
-#     if len(resultado2)==m:
-#         print(len(resultado2))
-
-            
 
         
-        
-birthday([1,1,1,1,1,1],3,2)
+birthday([1,2,3,4,5,6],3,2)
 #[1,2,1,3,2],3,2
 # ([4],4,1)
 #([1,1,1,1,1,1],3,2)
