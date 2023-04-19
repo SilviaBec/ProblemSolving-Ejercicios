7e839f92bb555e721bf34412a82b95a5583b0c58
Silvia Becerra <silvibecerra681@gmail.com>
2023-04-19 10:40:28 -0500

    Solucionado

--- PS4.js
+++ PS4.js
@@ -1,39 +1,28 @@
-// const ar=[1,3,2,6,1,2]
-// const k=3
-// var lista2=[]
-// for(var i of ar){
-//     for(let x=0; x< ar.length;x++){
-//         if(i<ar[x] && (ar[i]+ar[x])%k){
-//             var lista=[]
-//             lista.push(i,ar[x])
-//             console.log(lista)
-            
-//             }
-
-//         }
-//     }
-    
-    
-
-
-//node PS4.js
-
-
 
 
 const ar=[1,3,2,6,1,2]
 const k=3
 var lista2=[]
+let conta=0
 for(let i=0; i< ar.length;i++){ // 0 1 2 3 4 5
-    if(i<i+1 && (ar[i]+ar[i+1])%k){
-        var lista2=[]
-        lista2.push(i,i+1)
-        console.log(lista2)
+    for(let j=1;j< ar.length;j++){
+        if(i<j && ((ar[i]+ar[j])%k==0)){
+            lista2.push([i,j])
+            
+                
+            
+        }
     }
     
+
+    
 }
+for(let e of lista2){
+    conta=conta+1
+}
+console.log(conta)
         
         
     //no esta dando debe dar 
-    //1-2/1-2/3-6/
+    //0-2/0-5/1-3/2-4/4-5/
     //node PS4.js
