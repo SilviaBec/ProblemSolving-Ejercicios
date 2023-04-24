1fde2a7969872045e8107ef72a9a1608b9e142dd
SilviaBec <silvibecerra681@gmail.com>
2023-04-24 16:11:52 -0500

    Contador sin funcionar

--- PS5.py
+++ PS5.py
@@ -1,13 +1,13 @@
 
 def migratoryBirds(arr):
-    contador=0
+    contador=1
     for e in arr:
         for x in range(1,len(arr)):
-            if e==x:
+            if e==arr[x]:
                 contador=contador+1
-                print(e,contador)
+        print(e, contador)
 #necesito un contados cuantos hay de cada numero en el array
-# #cual es el mas repetido y si hay un empate 
+#cual es el mas repetido y si hay un empate 
 #entre 2, elegir el mas bajo.
 #answer here is 4 porque el 4 se repite 3 veces , la mayor frecuencia
 migratoryBirds([1,4,4,4,5,3])
