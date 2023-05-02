a4e4bcdf516d1e18214c2ee608e9c612fe4f70ba
SilviaBec <silvibecerra681@gmail.com>
2023-05-01 20:32:04 -0500

    lista de tuplas numero-frecuencia

--- PS5.py
+++ PS5.py
@@ -1,18 +1,22 @@
 
 def migratoryBirds(arr):
-    lista=[]
-    lista2=[]
+    frecuencias=[]
     contador=1
     for e in arr:
+        frecuencias.append(arr.count(e))
+        pares=zip(arr,frecuencias)
+    print(list(pares))
+    # print(list(pares))
+
         
-        if e not in lista:
-            lista.append(e)
+        # if e not in lista:
+        #     lista.append(e)
             
-        else:
-            lista2.append(e)
-    print(max(lista2))
+        # else:
+        #     lista2.append(e)
+        #     print(lista2.count(e))
             
-#necesito un contados cuantos hay de cada numero en el array
+#necesito un contador cuantos hay de cada numero en el array
 #cual es el mas repetido y si hay un empate 
 #entre 2, elegir el mas bajo.
 #answer here is 4 porque el 4 se repite 3 veces , la mayor frecuencia
