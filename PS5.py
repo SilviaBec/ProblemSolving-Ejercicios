8d51eaffc2831b8889c91fb18c04fff6b0d1b837
SilviaBec <silvibecerra681@gmail.com>
2023-05-11 06:45:14 -0500

    Nuevo abordaje al Problema

--- PS5.py
+++ PS5.py
@@ -2,14 +2,11 @@
 def migratoryBirds(arr):
     
     for e in arr:
-        for x in range(1,len(arr)):
-            if e == arr[x]:
-                frecuencia=0
-                frecuencia = frecuencia+1
-                print(frecuencia)
-            else:
-                frecuencia=1
-                print(frecuencia)
+        frecuencias= arr.count(e)
+        if frecuencias != 1:
+            print(frecuencias)
+            #Hasta aca voy super bien me imprime 2 2 las dos frecuencias mas repetidas
+    
 
         #     cuenta=0
         #     cuenta=cuenta+1
@@ -51,3 +48,7 @@ def migratoryBirds(arr):
 #entre 2, elegir el mas bajo.
 #answer here is 4 porque el 4 se repite 3 veces , la mayor frecuencia
 migratoryBirds([1,2,2,4])
+#El problema que debo tener aca [1,2,2,4] es que las frecuencias son  1 2 2 1
+# si elijo el mas bajo sale 1 pero 1 porque 1 esta dos veces pero no es un
+#num de frecuencia repetida sino 2 diferentes de frecuencia 1
+#arreglar eso!!!!!!!!!!!!
