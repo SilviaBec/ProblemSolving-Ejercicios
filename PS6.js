f8ca3fed3bb61e06ddde64df6dc7d3bb3bf8ff42
SilviaBec <silvibecerra681@gmail.com>
2023-08-14 08:56:29 -0500

    Desajuste mes

--- PS6.js
+++ PS6.js
@@ -30,18 +30,19 @@ abril=30, mayo= 31, junio= 30, julio= 31, agosto =31, septiembre= 30, octubre =3
 diciembre= 31 
 diasEneroaAgostoBisiesto=244;
 diasEneroaAgosto=243;
+var mes="";
+if(dia== 11 || dia==12 ||dia== 13 ||dia== 14){
+                                mes="09"
+                        } else {
+                                mes="08"
+                        }
 //243 dias hasta agosto 31 en un año bisiesto
 if (anio >= 1700 && anio <= 2700) {
         //Julian calendar
         if ((anio) >= 1700 && anio <= 1917) {
                 
                 
-                //ACA DIO -2 . 08 . 1918 DIA WRONG
-                if (anio == 1918) {
-                        dia = 256 - 244 - 14;
-                        console.log(dia, ".", "08", ".", anio);
-                }
-
+                
 
                 //EN TODOS LOS AÑOS DE ESTE INTERAVALO DA 12.09.AÑO
                 // In the Julian calendar, los años bisiestos son divisibles by 4;
@@ -49,10 +50,11 @@ if (anio >= 1700 && anio <= 2700) {
                         //256 dia programador
                         dia = 256 - 244; //12
                         //En bisiesto febrero tiene 29 dias
-                        console.log(dia, ".", "09", ".", anio);
+                        
+                        console.log(dia, ".", mes, ".", anio);
                 } else {
                         dia=256-243;
-                        console.log(dia, ".", "09", ".", anio);
+                        console.log(dia, ".", mes, ".", anio);
 
                 }
         }
@@ -60,12 +62,22 @@ if (anio >= 1700 && anio <= 2700) {
         // In the Gregorian calendar, los años bisiestos are Divisible by 400
         //or Divisible by 4 and not divisible by 100
         //RESULTADO  12 . 09 . 1920 EL DIA SIGUE MAL
+
+        //ACA DIO -2 . 08 . 1918 DIA WRONG
+                if (anio == 1918) {
+                        
+                        dia = 256 - 244 - 14;
+                        console.log(dia, ".", mes, ".", anio);
+                }
+
+
+
         else if ((anio) => 1919 && anio <= 2700) {
                 
                 if ((anio % 400 == 0 || anio % 4 == 0) && anio % 100 != 0) {
                         dia = 256 - 244; 
-                        console.log(dia,".","09",".",anio)
-                }else {console.log(dia, ".", "09", ".", anio)}
+                        console.log(dia,".",mes,".",anio)
+                }else {console.log(dia, ".", mes, ".", anio)}
         }
 }
 
