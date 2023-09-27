151e66da805ce50ee1685864e1cc11d8d1c01675
SilviaBec <silvibecerra681@gmail.com>
2023-09-27 14:07:31 -0500

    Avance

--- PS8.js
+++ PS8.js
@@ -1,8 +1,27 @@
 function pr(n,li){
-    for(let i=0; i<li.length;i++){
-        const resultado = li.filter((x)=>x==li[i])
-        console.log(resultado)
-    }
+    // for(let i=0; i<li.length;i++){
+    //     const resultado = li.filter((x)=>x==li[i])
+    //     console.log(resultado)
+    // }
+
+    let lista=[]
+    for( let i=0;i<li.length;i++){
+        if(lista.includes(li[i])==false){
+            lista.push(li[i])
+            
+        }
+        // lista.push(li[i])
+        
+    }console.log(lista)
+
+
+    //Eliminar duplicados
+    const lista2= new Set(li);
+    let resultado=[...lista2];
+    console.log(resultado)
+    // Contar el numero de elementos de resultado en la lista original
+    
+
 }
 
 pr(3,[2,1,2,1])
