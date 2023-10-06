afdf93a4aed3bbdfed00fda5ac66989a64385077
Silvia Becerra <silvibecerra681@gmail.com>
2023-10-06 11:08:03 -0500

    Error lógica

--- PS8.js
+++ PS8.js
@@ -13,21 +13,26 @@ function pr(n,li){
         }
         // lista.push(li[i])
         
-    }console.log(lista)
+    } //console.log(lista)
 
 
     //Eliminar duplicados
     const lista2= new Set(li);
     let resultado=[...lista2];
-    console.log(resultado)
-    // Contar el numero de elementos de resultado en la lista original
-    for(var i of resultado){
-        for(var x of li){
-            rta2=console.count(x)
+    //console.log(resultado)
+    //Contar el numero de elementos de resultado en la lista original
+    for(i of resultado){
+        var contador=contador+1
         }
+    } console.log(contador)
 
-    } console.log(rta2)
 
-}
+    // for(var i=0; of resultado){
+    //     for(var x of li){
+    //         rta2=console.count(x)
+    //     }
+
+    // } console.log(rta2)
+
 
 pr(3,[2,1,2,1])
