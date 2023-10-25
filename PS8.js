d15bb381ad6e23a7ecfdbb9acf05ceffc2394b74
SilviaBec <silvibecerra681@gmail.com>
2023-10-25 09:44:15 -0500

    Arreglado contador inicial

--- PS8.js
+++ PS8.js
@@ -3,40 +3,36 @@ function pr(n,li){
     let sinRep= new Set(ordenada);
     let enArray =Array.from(sinRep) //[ 1, 2, 3 ]
     sinRep=[sinRep]
-    let i;
+
     let contadorUno=0;
-    let contadorDos=0;
-    let contadorTres=0;
     // let contador=0;
 
-    for(let i of ordenada){ 
-        if(i==1){
-            contadorUno=contadorUno+1}
-        
-            }console.log(contadorUno)
-
-
-            for(let x of ordenada){ 
-                if(x==2){
-                    contadorDos=contadorDos+1}
-                
-                    }console.log(contadorDos)
-
-
-                    for(let w of ordenada){ 
-                        if(w==3){
-                            contadorTres=contadorTres+1}
-                        
-                            }console.log(contadorTres)
+    for(let x of enArray){ 
+        let cont=0;
+        for(let i of ordenada){
+            if(i==x){
+                cont=cont+1
+            }
+        }                          
+        //console.log(cont)
+    if(cont%2==0 || cont==1){
+        if(cont!==1){
+            console.log(cont/2)
+        }else{console.log(cont)}
+    }else{
+        console.log(cont-1)
+    }
+    
+}
             
-
-
-        } 
+} 
 
     
     
 
-// pr(9,[10, 20, 20, 10, 10, 30, 50, 10, 20])
+pr(9,[10, 20, 20, 10, 10, 30, 50, 10, 20])
 //debe dar 3 y da 9
-pr(11,[1,1,3,1,2,1,3,3,3,3,1]) //4
+//pr(11,[1,1,3,1,2,1,3,3,3,3,1]) //4
 //10 [1,1,3,1,2,1,3,3,3,3] debe dar 4 y me da 16
+
+//FALTA CONTADOR FINAL DE PARES¿?
