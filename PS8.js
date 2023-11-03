5dee704aa24998699d82bdd9d0c4a29255654f28
SilviaBec <silvibecerra681@gmail.com>
2023-11-03 09:18:45 -0500

    Terminado ! :)

--- PS8.js
+++ PS8.js
@@ -3,8 +3,8 @@ function pr(n,li){
     let sinRep= new Set(ordenada);
     let enArray =Array.from(sinRep) //[ 1, 2, 3 ]
     sinRep=[sinRep]
-    let pares=0;
-        let impares=0;
+    let acum=0;
+    
 
     for(let x of enArray){  //10, 20, 30, 50
         let cont=0;
@@ -15,27 +15,15 @@ function pr(n,li){
         }                          
         //console.log(cont) 
         
-        //HASTA ACA CUENTA CUANTOS HAY DE C/NUM 4 3 1 1 
-        //[10, 10, 10, 10, 20, 20, 20, 30, 50]
-
-    if(cont%2==0){//Si es par que lo divida en 2
-        pares=cont/2
+    if(cont%2==0){
+        acum=acum+(cont/2)
         
-    } else if(cont%2!==0 && (cont-1)!==0 ){//Si es impar y != de 1
-        impares= (cont-1)/2
+    } else if(cont%2!==0 && (cont-1)!==0 ){
+        acum= acum+((cont-1)/2)
         }
         
-    }console.log(pares+impares)
+    }console.log(acum)
 }
     
-//DEBO SUMAR LAS PAREJAS Y DAR UN SOLO NUM
-//NO ESTA FUNCIONANDO CON LA SEGUNDA tanda
-            
-
-    
-
-//pr(9,[10, 20, 20, 10, 10, 30, 50, 10, 20])
-//debe dar 3 y da 9
-pr(11,[1,1,3,1,2,1,3,3,3,3,1]) //4
-//10 [1,1,3,1,2,1,3,3,3,3] debe dar 4 y me da 16
-//FALTA CONTADOR FINAL DE PARES¿?
+pr(9,[10, 20, 20, 10, 10, 30, 50, 10, 20])
+//debe dar 3 
