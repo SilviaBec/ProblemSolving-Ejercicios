65f6de678dae1a739c2ab5c6514865abb8272c7f
SilviaBec <silvibecerra681@gmail.com>
2023-11-02 20:45:49 -0500

    Falta suma

--- PS8.js
+++ PS8.js
@@ -3,6 +3,8 @@ function pr(n,li){
     let sinRep= new Set(ordenada);
     let enArray =Array.from(sinRep) //[ 1, 2, 3 ]
     sinRep=[sinRep]
+    let pares=0;
+        let impares=0;
 
     for(let x of enArray){  //10, 20, 30, 50
         let cont=0;
@@ -11,23 +13,29 @@ function pr(n,li){
                 cont=cont+1
             }
         }                          
-        //console.log(cont) HASTA ACA CUENTA CUANTOS HAY DE C/NUM
-    if(cont%2==0){
-        console.log(cont/2)
-    }else if(cont%2 !==0 && cont-1!==0 ){
-        console.log(cont-1)
+        //console.log(cont) 
+        
+        //HASTA ACA CUENTA CUANTOS HAY DE C/NUM 4 3 1 1 
+        //[10, 10, 10, 10, 20, 20, 20, 30, 50]
+
+    if(cont%2==0){//Si es par que lo divida en 2
+        pares=cont/2
+        
+    } else if(cont%2!==0 && (cont-1)!==0 ){//Si es impar y != de 1
+        impares= (cont-1)/2
         }
-    }
+        
+    }console.log(pares+impares)
 }
     
-//DA DOS QUE SON 2 POR LAS 2 PAREJAS DE 10 PERO 2 DEL 3-1 QUE ES UNA SOLA PAREJA DE 20S ARREGLAR ESO
-
+//DEBO SUMAR LAS PAREJAS Y DAR UN SOLO NUM
+//NO ESTA FUNCIONANDO CON LA SEGUNDA tanda
             
 
     
 
-pr(9,[10, 20, 20, 10, 10, 30, 50, 10, 20])
+//pr(9,[10, 20, 20, 10, 10, 30, 50, 10, 20])
 //debe dar 3 y da 9
-//pr(11,[1,1,3,1,2,1,3,3,3,3,1]) //4
+pr(11,[1,1,3,1,2,1,3,3,3,3,1]) //4
 //10 [1,1,3,1,2,1,3,3,3,3] debe dar 4 y me da 16
 //FALTA CONTADOR FINAL DE PARES¿?
