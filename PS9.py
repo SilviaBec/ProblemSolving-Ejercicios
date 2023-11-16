fdb37212418563ec192f60f7e7f90c681580b298
SilviaBec <silvibecerra681@gmail.com>
2023-11-15 19:09:39 -0500

    Inicio esqueleto

---
+++ PS9.py
@@ -0,0 +1,11 @@
+def libro(pagi,num):
+    if pagi>=num/2:
+        print('Desde el final')
+    else:
+        print('Desde el inicio')
+
+    for x in range(2,num):
+        pass
+
+
+libro(6,2) #Debe dar 1
