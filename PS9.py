571d5664d160e1d049451b957a2f8885ddf150eb
SilviaBec <silvibecerra681@gmail.com>
2023-11-20 13:08:13 -0500

    error logico

--- PS9.py
+++ PS9.py
@@ -6,22 +6,27 @@ def libro(pagi,num):
 
     if (num+2)%2==0:
         numDepares=(num+2)/2
+        print((num+2),(num+2)/2)
     else:
         numDepares=(num+1)/2
-
+        print((num+1),(num+2)/2)
     if pagi==numDepares/2:
         resultado=1
+        print('1')
     elif pagi<numDepares/2:
         resultado=(numDepares/2)-pagi
         print(resultado/2)
+        print('2')
     else:
-        resultado=(pagi-(numDepares/2))
+        resultado=(pagi-numDepares)
+        print('3')
     
-    print(resultado)
+    print((resultado/2)-1)
     
     
 
 
 
 
-libro(6,2) #Debe dar 1
+#libro(6,2) #Debe dar 1
+libro(9,2) #Debe dar 1
