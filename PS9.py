d7e165ca40c65efb0b8a24abdb818e77d81ff9de
SilviaBec <silvibecerra681@gmail.com>
2023-11-22 11:28:59 -0500

    reajuste logica

--- PS9.py
+++ PS9.py
@@ -1,32 +1,27 @@
-def libro(pagi,num):
+#revisar bien el orden de los parametros porque tenia el num de segundas y lo 
+#llamaba como si fuera pag, abajo al llamar la funcion igual , revisar que si
+# pongo num, pag abajo ponga el valor de num, valor pag
+def libro(num,pagi):
     if pagi>=num/2:
         print('Desde el final')
     else:
         print('Desde el inicio')
 
-    if (num+2)%2==0:
-        numDepares=(num+2)/2
-        print((num+2),(num+2)/2)
-    else:
-        numDepares=(num+1)/2
-        print((num+1),(num+2)/2)
-    if pagi==numDepares/2:
+        if num%2 ==0:
+            num=num+2
+            numDepares=(num)/2
+            mitadPares=numDepares/2
+            print(int(numDepares),int(mitadPares))
+        else:
+            num=num+1
+            numDepares=num/2
+            mitadPares=numDepares/2
+            print(numDepares,mitadPares)
+
+    if pagi-mitadPares==0:
         resultado=1
-        print('1')
-    elif pagi<numDepares/2:
-        resultado=(numDepares/2)-pagi
-        print(resultado/2)
-        print('2')
-    else:
-        resultado=(pagi-numDepares)
-        print('3')
+        print(resultado) #esto aca no imprime 
     
-    print((resultado/2)-1)
-    
-    
-
-
-
 
 #libro(6,2) #Debe dar 1
-libro(9,2) #Debe dar 1
+libro(8,2) #Debe dar 1
